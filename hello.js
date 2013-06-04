7f3c3118913a3d57575da65d8e7b0750a94c9f7c
syam sasikumar <syamsasikumar@gmail.com>
2013-06-04 16:38:01 -0400

    setting response

--- hello.js
+++ hello.js
@@ -4,14 +4,14 @@ var port = process.env.PORT || 8000;
 
 request('http://www.google.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-	var server = http.createServer(function (request, response) {
-	  response.writeHead(200);
-	  response.end(body);
+	var server = http.createServer(function (req, res) {
+	  res.writeHead(200);
+	  res.end(body);
 	});
+	server.listen(port);
   }
 });
 
-server.listen(port);
 
 
 
