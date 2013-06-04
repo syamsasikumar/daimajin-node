8618ec3983197bd365fdcb4a1f904933dbf3b145
syam sasikumar <syamsasikumar@gmail.com>
2013-06-04 16:35:48 -0400

    setting response

--- hello.js
+++ hello.js
@@ -1,6 +1,17 @@
 var request = require('request');
+var http = require('http');
+var port = process.env.PORT || 8000;
+
 request('http://www.google.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log("Test for http client") // Print the google web page.
+	var server = http.createServer(function (request, response) {
+	  response.writeHead(200);
+	  response.end(body);
+	});
   }
-})
+});
+
+server.listen(port);
+
+
+
