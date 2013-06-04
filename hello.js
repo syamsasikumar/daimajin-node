ccc66e49ca1b907db54fad30935a14322e2f9d75
syam sasikumar <syamsasikumar@gmail.com>
2013-06-04 16:42:13 -0400

    setting response

--- hello.js
+++ hello.js
@@ -1,8 +1,9 @@
 var request = require('request');
 var http = require('http');
 var port = process.env.PORT || 8000;
+var url = "http://www.google.com"; //RT api call here
 
-request('http://www.google.com', function (error, response, body) {
+request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
 	var server = http.createServer(function (req, res) {
 	  res.writeHead(200);
