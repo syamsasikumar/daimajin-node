6e81120154c1abca01ee735ade20a1d831f75e2d
syam sasikumar <syamsasikumar@gmail.com>
2013-06-10 20:41:41 -0400

    changed to using tmdb. RT unstable

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var request = require('request');
 var http = require('http');
 var port = process.env.PORT || 8080;
-var api_key = '8j6p4xyu8mrkfsf65tzeyktn'; // @TODO: hide me !
-var movie_url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey='+ api_key; //RT movie query api
-var resource_url = 'http://api.rottentomatoes.com/api/public/v1.0/movies/<resource>.json?apikey=' + api_key; //RT movie resource api
+var api_key = '2b8c09380433776e2676d7a7ef694d48';// @TODO: hide me !
+var movie_url = 'http://api.themoviedb.org/3/search/movie?api_key='+ api_key; //RT movie query api
+var resource_url = 'http://api.themoviedb.org/3/movie/<resource>?api_key=' + api_key; //RT movie resource api
 var url = require('url');
 
 
 var server = http.createServer(function (req, res) {
   var parts = url.parse(req.url, true).query;
   var q = parts.q;
-  var page_limit = parts.total == undefined? 5: parts.total;
   var page = parts.page == undefined? 1: parts.page;
   var id = parts.movie;
 
@@ -24,12 +23,21 @@ var server = http.createServer(function (req, res) {
     if(id != undefined){
       api_url = resource_url.replace('<resource>',id);
     }else{
-      api_url = movie_url + '&q=' + q.split(' ').join('+') + '&page=' + page + '&page_limit=' + page_limit;
+      api_url = movie_url + '&query=' + q.split(' ').join('+') + '&page=' + page;
     }
-    request(api_url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.end(body);
-      }
+    request(
+      {
+        uri:api_url,
+        headers:{
+          accept: 'application/json'
+        }
+      }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          res.end(body);
+        }
+        else{
+          res.end(JSON.stringify({}));
+        }
     });
   }
 });
