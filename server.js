2b645a766c846620f1336b65d6046195cca3b4f0
syam sasikumar <syamsasikumar@gmail.com>
2013-06-23 16:19:11 -0400

    added response header for cross origin ajax

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var http = require('http');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8081;
 var api_key = '2b8c09380433776e2676d7a7ef694d48';// @TODO: hide me !
 var movie_url = 'http://api.themoviedb.org/3/search/movie?api_key='+ api_key + '&search_type=ngram'; //TMDB movie query api
 var resource_url = 'http://api.themoviedb.org/3/movie/<resource>?api_key=' + api_key; //TMDB movie resource api
@@ -14,7 +14,7 @@ var server = http.createServer(function (req, res) {
   var id = parts.movie;
 
   res.writeHead(200);
-  res.writeHead(200, {"Content-Type": "application/json"});
+  res.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers' : 'Origin, X-Requested-With, Content-Type, Accept'});
 
   if(q == undefined && id == undefined){
     res.end(JSON.stringify({}));
