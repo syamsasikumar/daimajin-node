2a8be6ff2df5f03fced952a5121d7aeadc76698b
syam sasikumar <syamsasikumar@gmail.com>
2013-07-10 13:56:04 -0400

    fetch movies for my ratings page

--- routes/ratings.js
+++ routes/ratings.js
@@ -8,31 +8,49 @@ var conf = require('../helpers/conf'),
   headers = { 
     accept: 'application/json' 
   },
-  ratingsCollection = 'rating';
+  ratingsCollection = 'rating',
+  moviesCollection = 'movie';
 
 var db = new database();
 
 exports.get = function(req, res){
   var id = req.params.id;
+  var getMovies = req.query.movies;
   if(id){
     db.getCollection(ratingsCollection, function(collection){
       if(collection){
         collection.findOne({userId: id}, {ratings:1}, function(err, item){
           if(err){
-            res.send({ code:1, user:id, ratings:{}});
+            res.send({ code:1, user:id, ratings:{}, movies:{}});
           }
           if(item){
-            res.send({code:0, user:id, ratings:item.ratings});
+            var ratings = item.ratings;
+            if(getMovies && ratings){
+              var movies = [];
+              var mids = Object.keys(JSON.parse(ratings));
+              db.getCollection(moviesCollection, function(collection){
+                if(collection){
+                  for(var i=0; i<mids.length; i++) { mids[i] = parseInt(mids[i]); } // convert to array of ints
+                  collection.find({'id': { $in: mids}}, {id:1, belongs_to_collection:1, backdrop_path:1, genres:1 , original_title:1, poster_path:1, release_date:1 }).toArray(function(err, movies){
+                    res.send({code:0, user:id, ratings: ratings, movies: movies});
+                  });
+                }else{
+                  res.send({code:0, user:id, ratings: ratings, movies:movies});
+                }
+              });
+            }else{
+              res.send({code:0, user:id, ratings:ratings, movies:{}});
+            }
           }else{
-            res.send({code:0, user:id, ratings:{}});
+            res.send({code:0, user:id, ratings:{}, movies:{}});
           }
         });
       }else{
-        res.send({code:1, user:id, ratings:{}});
+        res.send({code:1, user:id, ratings:{}, movies:{}});
       }
     });
   }else{
-    res.send({code:1, user:id, ratings:{}});
+    res.send({code:1, user:id, ratings:{}, movies:{}});
   }
 }
 
